33d6aa362e06bfb98d70b9694dca5eb05722675a
mathurinm <mathurin.massias@gmail.com>
2022-10-09 10:52:00 +0200

    add objective

--- objective.py
+++ objective.py
@@ -1,38 +1,31 @@
 from benchopt import BaseObjective, safe_import_context
 
-# Protect import to allow manipulating objective without importing library
-# Useful for autocompletion and install commands
 with safe_import_context() as import_ctx:
     import numpy as np
+    from numpy.linalg import norm
 
 
 class Objective(BaseObjective):
-    name = "Ordinary Least Squares"
-
-    # All parameters 'p' defined here are available as 'self.p'
+    name = "Multitask Lasso"
     parameters = {
-        'fit_intercept': [False],
+        "reg": [1, 0.5, 0.1, 0.01],
     }
 
-    def get_one_solution(self):
-        # Return one solution. This should be compatible with 'self.compute'.
-        return np.zeros(self.X.shape[1])
+    def __init__(self, reg=0.1):
+        self.reg = reg
 
-    def set_data(self, X, y):
-        # The keyword arguments of this function are the keys of the `data`
-        # dict in the `get_data` function of the dataset.
-        # They are customizable.
-        self.X, self.y = X, y
+    def set_data(self, X, Y):
+        self.X, self.Y = X, Y
+        # todo normalize by n_samples?
+        self.alpha_max = np.max(norm(X.T @ Y, axis=1))
+        self.lmbd = self.reg * self.alpha_max
 
-    def compute(self, beta):
-        # The arguments of this function are the outputs of the
-        # `get_result` method of the solver.
-        # They are customizable.
-        diff = self.y - self.X.dot(beta)
-        return .5 * diff.dot(diff)
+    def compute(self, W):
+        R = self.Y - self.X @ W
+        p_obj = 0.5 * norm(R, ord="fro") ** 2 + self.lmbd * norm(
+            W, axis=1).sum()
+        nnz = np.sum(norm(W, axis=1) != 0)
+        return dict(value=p_obj, sparsity=nnz)
 
     def to_dict(self):
-        # The output of this function are the keyword arguments
-        # for the `set_objective` method of the solver.
-        # They are customizable.
-        return dict(X=self.X, y=self.y, fit_intercept=self.fit_intercept)
+        return dict(X=self.X, Y=self.Y, lmbd=self.lmbd)
